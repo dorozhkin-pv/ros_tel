df77ab22a7812a2d083b0167e04d2d6b8e9608b6
Makhnev Pavel <pavel.makhnev@bk.ru>
2019-09-28 13:48:25 +0300

    map

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,21 @@
 <template>
     <div class="home">
-        <h1>Начни думать о планете сегодня!</h1>
-
         <h3>Выберите тип мусора для утилизации</h3>
-
         <div class="my-form mt-5">
-            <form @submit.prevent>
-                <div class="form-check" v-for="(item, index) in tags">
-                    <div class="my-img" :style="'background-position: 30px 30px'"></div>
-                    <input class="form-check-input"
-                           :id="item.tag_id"
-                           :key="index"
-                           type="checkbox"
-                           :value="item.tag_id"
-                           @change="onChange(item.tag_id, $event.target)">
-                    <label class="form-check-label" :for="index">{{ item.name }}
-                    </label>
+            <div class="flexmy">
+                <div class="form-check" :class="(checked.indexOf(item.tag_id) > -1 ? 'checked' : 'not-checked')+' '+item.image" v-for="(item, index) in tags" @click="checkButton(item.tag_id)">
+                    <div class="help" @click.preventDefault="header={header: item.name, description: item.description}">?</div>
                 </div>
+            </div>
+        </div>
 
-                <button type="submit" class="btn btn-primary mt-2" @click="goToCard">Найти</button>
-            </form>
+        <button type="submit" class="btn-full" @click="goToCard">Найти</button>
+
+        <div class="description" v-if="header">
+            <h1>{{ header.header }}</h1>
+            <div class="close" @click="header = false;">X</div>
+
+            <p>{{ header.description }}</p>
         </div>
 
     </div>
@@ -31,17 +27,20 @@ export default {
         return {
             selected: this.$store.getters.typeOfGarbage,
             checked: [],
-            tags: []
+            tags: [],
+            header: false
         }
     },
     mounted() {
         fetch("/api/v1/tag").then(response => response.json()).then(data => this.setTags(data.data));
     },
     methods: {
-        onChange(index, event){
-            this.$set(this.checked, index, event.value);  //отослать на сервер
-
-            console.log(this.checked);
+        checkButton(value){
+            if(this.checked.indexOf(value) != -1){
+                this.checked.splice(this.checked.indexOf(value), 1);
+            } else {
+                this.checked.push(value);
+            }
 
             this.$store.commit('setTypeOfGarbage', this.checked);
         },
@@ -60,9 +59,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
     .home {
-        height: calc(100vh - 56px);
-        background: url('../assets/main.jpg') no-repeat center;
-        background-size: cover;
+
+        height:100%;
     }
     .my-img {
         position: absolute;
@@ -82,11 +80,135 @@ export default {
             flex-direction: column;
             align-items: flex-start;
         }
+
+
     }
+
+    .flexmy {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        height: 391px;
+        overflow: auto;
+        width:100%;
+    }
+
+    .form-check {
+        width:33.3%;
+        height:100px;
+        mix-blend-mode: multiply;
+    }
+
+    .checked {
+
+    }
+
+    .not-checked {
+        filter: grayscale(100%);
+    }
+
+    .aero.not-checked {
+        opacity: 0.5;
+    }
+
+    .battery.not-checked {
+        opacity: 0.5;
+    }
+
+    .aero {
+        background-image: url('../assets/trash/aero.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .thermo {
+        background-image: url('../assets/trash/thermo.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .battery {
+        background-image: url('../assets/trash/battery.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .chemy {
+        background-image: url('../assets/trash/chemy.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .metal {
+        background-image: url('../assets/trash/metal.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .organic {
+        background-image: url('../assets/trash/oraganic.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .paper {
+        background-image: url('../assets/trash/paper.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .plastic {
+        background-image: url('../assets/trash/plastic.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    .glass {
+        background-image: url('../assets/trash/glass.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    .btn-full {
+        width: 100%;
+        height: 50px;
+        color: black;
+        background-color: green;
+        position: absolute;
+        bottom: 0;
+        left:0;
+    }
+
     .form-check {
         margin-bottom: 30px;
     }
+
+    .help {
+        width: 20px;
+        height: 20px;
+        border: 1px solid black;
+        border-radius: 15px;
+        font-size: 13px;
+    }
+
     .btn {
         /* align-self: center; */
+    }
+
+    .description {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 9999;
+        left: 0;
+        top: 0;
+        background-color: white;
+    }
+
+    .close {
+        position: absolute;
+        left: 90%;
+        top: 10px;
     }
 </style>
